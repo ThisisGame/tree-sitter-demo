7e57baabba5c35257460214f4331e467ee63f077
captainchen <captainchen@tencent.com>
2023-11-24 21:15:07 +0800

    cpp

--- test-cpp/main.cpp
+++ test-cpp/main.cpp
@@ -10,6 +10,20 @@
 #include <iomanip>
 #include <sstream>
 
+#define InsertTraceToFunction 0
+
+#define ParseFunction 1
+
+#define PRINT_MSG(...) \
+    std::cout << __VA_ARGS__ << std::endl; \
+	log_file << __VA_ARGS__ << std::endl;
+
+#define NODE_ERROR_CONTINUE(NodeName,NodeCode) \
+															log_file << NodeName << " has error--->\n" << NodeCode << std::endl; \
+															std::cout<<"\033[1;31m"<<NodeName<<" has error--->\033[0m\n"<<NodeCode<<std::endl; \
+															node = ts_node_next_named_sibling(node); \
+															continue;
+
 extern "C" TSLanguage *tree_sitter_cpp();
 
 // 遍历目录并找到所有的 .cpp 文件
@@ -76,68 +90,84 @@ TSNode ts_find_node_by_type(TSNode node, const char* node_type) {
  * \param source_code 源代码字符串
  * \param insertions 保存需要插入的字符串和位置的向量
  */
-void traverse_and_print(TSNode node, const std::string& source_code, std::vector<std::pair<size_t, std::string>>& insertions) {
+void traverse_and_print(TSNode node, const std::string& source_code, std::vector<std::pair<size_t, std::string>>& insertions,std::ofstream& log_file) {
     while (ts_node_is_null(node) == false) {
         // 打印节点的类型
         const char* node_type = ts_node_type(node);
-        //printf("Node type: %s\n", node_type);
+        //PRINT_MSG("Node type: "<<node_type)
 
-         std::string function_code = source_code.substr(ts_node_start_byte(node), ts_node_end_byte(node) - ts_node_start_byte(node));
+    	std::string node_code = source_code.substr(ts_node_start_byte(node), ts_node_end_byte(node) - ts_node_start_byte(node));
+        //PRINT_MSG("Node code: "<<node_code)
 
+#if ParseFunction
         // 如果节点是函数，添加TRACE_CPUPROFILER_EVENT_SCOPE
         if (strcmp(node_type, "function_definition") == 0) {
+
+            // 判断Node是否包含Error
+            if(ts_node_has_error(node))
+			{
+				NODE_ERROR_CONTINUE("function_definition",node_code)
+			}
 
             // 获取函数定义
             TSNode function_declarator_node = ts_find_node_by_type(node, "function_declarator");
             if(ts_node_is_null(function_declarator_node))
             {
-                std::cout<<"\033[1;31mfunction_declarator_node null--->\033[0m\n"<<function_code<<std::endl;
-                node = ts_node_next_named_sibling(node);
-                continue;
+                NODE_ERROR_CONTINUE("function_declarator",node_code)
             }
             std::string function_declarator_node_code = source_code.substr(ts_node_start_byte(function_declarator_node), ts_node_end_byte(function_declarator_node) - ts_node_start_byte(function_declarator_node));
-
-            // 获取函数名
-            TSNode function_name_node = ts_node_child_by_field_name(function_declarator_node, "declarator", strlen("declarator"));
-            if(ts_node_is_null(function_name_node))
-            {
-                std::cout<<"\033[1;31mfunction_name_node null--->\033[0m\n"<<function_code<<std::endl;
-                node = ts_node_next_named_sibling(node);
-                continue;
-            }
-            std::string function_name = source_code.substr(ts_node_start_byte(function_name_node), ts_node_end_byte(function_name_node) - ts_node_start_byte(function_name_node));
 
             //获取函数体compound_statement
             TSNode compound_statement_node = ts_node_child_by_node_type(node, "compound_statement");
         	if(ts_node_is_null(compound_statement_node))
             {
-                std::cout<<"\033[1;31mcompound_statement_node null--->\033[0m\n"<<function_code<<std::endl;
-                node = ts_node_next_named_sibling(node);
-                continue;
+                NODE_ERROR_CONTINUE("compound_statement",node_code)
             }
             std::string compound_statement_node_code = source_code.substr(ts_node_start_byte(compound_statement_node), ts_node_end_byte(compound_statement_node) - ts_node_start_byte(compound_statement_node));
 
-            //获取函数体的第一个child node
+            //获取函数体的第一个child node，在它插入代码
             if (ts_node_child_count(compound_statement_node) > 1) {
                 TSNode first_child_node = ts_node_child(compound_statement_node, 1);
                 if(ts_node_is_null(first_child_node))
 	            {
-                    std::cout<<"\033[1;31mfirst_child_node null--->\033[0m\n"<<function_code<<std::endl;
-	                node = ts_node_next_named_sibling(node);
-	                continue;
+	                NODE_ERROR_CONTINUE("first_child_node",node_code)
 	            }
                 std::string first_child_node_code = source_code.substr(ts_node_start_byte(first_child_node), ts_node_end_byte(first_child_node) - ts_node_start_byte(first_child_node));
 
                 // 获取开始位置
 				uint32_t first_child_start = ts_node_start_byte(first_child_node);
-	            std::string trace_line = "TRACE_CPUPROFILER_EVENT_SCOPE(" + function_name + ");\n\t";
+
+                // 获取函数名
+	            TSNode function_name_node = ts_node_child_by_field_name(function_declarator_node, "declarator", strlen("declarator"));
+	            if(ts_node_is_null(function_name_node))
+	            {
+	                NODE_ERROR_CONTINUE("function_name_node",node_code)
+	            }
+	            std::string function_name = source_code.substr(ts_node_start_byte(function_name_node), ts_node_end_byte(function_name_node) - ts_node_start_byte(function_name_node));
+
+                // 判断函数名是否异常(是否有多行)
+				if(function_name.find('\n') != std::string::npos)
+				{
+					NODE_ERROR_CONTINUE("function_name multiline",function_name)
+				}
+
+#if InsertTraceToFunction
+
+                // 获取函数体与第一个Node之间的空白字符
+                std::string blank_chars = source_code.substr(ts_node_start_byte(compound_statement_node)+1, ts_node_start_byte(first_child_node) - ts_node_start_byte(compound_statement_node)-1);
+
+
+	            std::string trace_line = "TRACE_CPUPROFILER_EVENT_SCOPE(" + function_name + ");";
+                trace_line += blank_chars;
 	            insertions.push_back({first_child_start, trace_line});
+#endif
             }
         }
+#endif
 
         // 如果节点有子节点，递归遍历
         if (ts_node_child_count(node) > 0) {
-            traverse_and_print(ts_node_named_child(node, 0), source_code, insertions);
+            traverse_and_print(ts_node_named_child(node, 0), source_code, insertions,log_file);
         }
 
         // 获取下一个节点
@@ -145,7 +175,15 @@ void traverse_and_print(TSNode node, const std::string& source_code, std::vector
     }
 }
 
+
+
 int main(int argc, char* argv[]) {
+    std::time_t t = std::time(nullptr);
+	char buf[100];
+	std::strftime(buf, sizeof(buf), "%Y-%m-%d-%H-%M-%S", std::localtime(&t));
+	std::string filename = std::string("log-") + buf;
+	std::ofstream log_file(filename, std::ios_base::app);
+
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <directory>\n";
         return 1;
@@ -160,11 +198,13 @@ int main(int argc, char* argv[]) {
     // 找到所有的 .cpp 文件
     std::vector<std::string> cpp_files = find_cpp_files(argv[1]);
 
+#if InsertTraceToFunction
     // 获取目录名
     std::string dir_name = std::filesystem::path(argv[1]).filename().string();
 
     // 备份 .cpp 文件
     backup_files(cpp_files, std::filesystem::path(argv[1]).parent_path().string() + "/" + dir_name + "_bak_" + ss.str());
+#endif
 
     // 创建一个解析器
     TSParser *parser = ts_parser_new();
@@ -174,11 +214,11 @@ int main(int argc, char* argv[]) {
 
     // 遍历并处理所有的 .cpp 文件
     for (const auto& file_path : cpp_files) {
-        std::cout<<file_path<<std::endl;
+        PRINT_MSG(file_path)
+
         // 解析源代码
         std::ifstream file(file_path);
         if (!file) {
-            std::cout << "Failed to open file: " << file_path << "\n";
             continue;
         }
         std::string source_code((std::istreambuf_iterator<char>(file)),
@@ -191,8 +231,9 @@ int main(int argc, char* argv[]) {
 
         // 遍历抽象语法树并记录需要插入的字符串和位置
         std::vector<std::pair<size_t, std::string>> insertions;
-        traverse_and_print(root_node, source_code, insertions);
+        traverse_and_print(root_node, source_code, insertions,log_file);
 
+#if InsertTraceToFunction
         // 按照位置从大到小的顺序插入字符串，这样不会影响到其他插入位置的正确性
         std::sort(insertions.begin(), insertions.end(), [](const std::pair<size_t, std::string>& a, const std::pair<size_t, std::string>& b) {
             return a.first > b.first;
@@ -202,10 +243,12 @@ int main(int argc, char* argv[]) {
             source_code.insert(insertion.first, insertion.second);
         }
 
+
         // 覆盖原始文件
         std::ofstream out_file(file_path);
         out_file << source_code;
         out_file.close();
+#endif
 
         // 删除抽象语法树
         ts_tree_delete(tree);
